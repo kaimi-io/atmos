656fa83aa793978e3e5bb3b110626d89e20d7571
d-x <rukaimi@gmail.com>
2017-09-09 18:40:35 +0300

    Added forward_list and some useful defines

---
+++ atmos/kernel/defines.h
@@ -0,0 +1,28 @@
+#pragma once
+
+///Packed structs/classes (with 1-byte aligned members).
+#define ATMOS_PACKED __attribute__((packed))
+
+///Functions that do not return.
+#define ATMOS_NORETURN __attribute__((noreturn))
+
+///Functions without parameters, prologue/epilogue and return instruction.
+#define ATMOS_NAKED __attribute__((naked))
+
+///Often called functions.
+#define ATMOS_HOT __attribute__((hot))
+
+///Rarely called functions.
+#define ATMOS_COLD __attribute__((cold))
+
+///Used functions.
+#define ATMOS_USED __attribute__((used))
+
+///OS task functions - do not save call-saved registers, interrupt-safe.
+#define ATMOS_OS_TASK __attribute__((OS_task))
+
+///OS main functions - do not save call-saved registers, interrupt-unsafe.
+#define ATMOS_OS_MAIN __attribute__((OS_main))
+
+///Non-null function parameters.
+#define ATMOS_NONNULL(...) __attribute__((nonnull(__VA_ARGS__)))
---
+++ atmos/kernel/forward_list.h
@@ -0,0 +1,220 @@
+#pragma once
+
+#include "defines.h"
+
+namespace atmos
+{
+namespace container
+{
+
+///Base class for strongly typed element of singly-linked list.
+///Can be contained in single list only.
+struct ATMOS_PACKED forward_list_element
+{
+	///Next list item or nullptr.
+	forward_list_element* next = nullptr;
+};
+
+///Base class for strongly typed singly-linked list.
+struct ATMOS_PACKED forward_list_base
+{
+	///Pointer to first list element or nullptr if list is empty.
+	forward_list_element* first_element = nullptr;
+	
+	///<summary>Push new element to front of list.</summary>
+	///<param name="elem">Element not attached to any list. Can not be nullptr.</param>
+	void push_front(forward_list_element* elem) ATMOS_NONNULL(1);
+
+	///<summary>Pop front element of list.</summary>
+	///<returns>Front list element or nullptr if list is empty.</returns>
+	forward_list_element* pop_front();
+	
+	///<summary>Remove element from list.</summary>
+	///<remarks>Has O(n) time complexity.</remarks>
+	///<param name="elem">Any element of any list. Can not be nullptr.</param>
+	///<returns>True if element was removed. False if element was not found in list.</returns>
+	bool remove(forward_list_element* elem) ATMOS_NONNULL(1);
+	
+	///<summary>Insert element "elem" before element "current".</summary>
+	///<param name="prev">Element which is before "current" element. Can be nullptr.</param>
+	///<param name="current">Element after which "elem" element must be inserted. Can not be nullptr.</param>
+	///<param name="elem">Element not attached to any list. Can not be nullptr.</param>
+	void insert_before(forward_list_element* prev, forward_list_element* current, forward_list_element* elem) ATMOS_NONNULL(2, 3);
+	
+	///<summary>Returns true if list is empty.</summary>
+	///<returns>True if list is empty, false otherwise.</returns>
+	bool empty() const;
+	
+	///<summary>Returns first element of the list or nullptr if list is empty.</summary>
+	///<returns>First element of the list or nullptr if list is empty.</returns>
+	forward_list_element* first();
+};
+
+void forward_list_base::push_front(forward_list_element* elem)
+{
+	elem->next = first_element;
+	first_element = elem;
+}
+
+forward_list_element* forward_list_base::pop_front()
+{
+	auto* result = first_element;
+	if(result)
+		first_element = result->next;
+	return result;
+}
+
+bool forward_list_base::remove(forward_list_element* elem)
+{
+	auto* current = first_element;
+	forward_list_element* prev = nullptr;
+	while(current != elem)
+	{
+		if(!current)
+			return false;
+			
+		prev = current;
+		current = current->next;
+	}
+		
+	if(prev)
+		prev->next = elem->next;
+	else
+		first_element = elem->next;
+		
+	elem->next = nullptr;
+	return true;
+}
+
+void forward_list_base::insert_before(forward_list_element* prev, forward_list_element* current, forward_list_element* elem)
+{
+	elem->next = current;
+		
+	if(prev)
+		prev->next = elem;
+	else
+		first_element = elem;
+}
+
+bool forward_list_base::empty() const
+{
+	return !first_element;
+}
+
+forward_list_element* forward_list_base::first()
+{
+	return first_element;
+}
+
+///Base class for typed element of singly-linked list. Supports tags, allowing element to be in several different
+///singly-linked lists simultaneously.
+template<typename Tag, typename ContainedType>
+struct ATMOS_PACKED forward_list_element_tagged : forward_list_element
+{
+	using tag_type = Tag;
+	using contained_type = ContainedType;
+	using list_element_type = forward_list_element_tagged<tag_type, contained_type>;
+
+	///<summary>Downcasts list element to contained element type.</summary>
+	///<returns>Contained element pointer.</returns>
+	contained_type* operator->()
+	{
+		return static_cast<contained_type*>(this);
+	}
+};
+
+///Typed singly-linked list which allows its typed contained elements to be contained in several different lists.
+template<typename ForwardListElement>
+struct ATMOS_PACKED forward_list_tagged : private forward_list_base
+{
+	using list_element_type = ForwardListElement;
+	using tag_type = typename list_element_type::tag_type;
+	using contained_type = typename list_element_type::contained_type;
+
+	using forward_list_base::empty;
+
+	///<summary>Push new element to front of list.</summary>
+	///<param name="elem">Element not attached to any list of the same type. Can not be nullptr.</param>
+	void push_front(list_element_type* elem) ATMOS_NONNULL(1);
+
+	///<summary>Pop front element of list.</summary>
+	///<returns>Front list element or nullptr if list is empty.</returns>
+	list_element_type* pop_front();
+
+	///<summary>Remove element from list.</summary>
+	///<remarks>Has O(n) time complexity.</remarks>
+	///<param name="elem">Any element of any list. Can not be nullptr.</param>
+	///<returns>True if element was removed. False if element was not found in list.</returns>
+	bool remove(list_element_type* elem) ATMOS_NONNULL(1);
+	
+	///<summary>Returns first element of the list or nullptr if list is empty.</summary>
+	///<returns>First element of the list or nullptr if list is empty.</returns>
+	list_element_type* first();
+
+	///<summary>Insert element "elem" before any element that satisfies the condition.</summary>
+	///<remarks>If list is empty, element "elem" is just inserted.</remarks>
+	///<param name="elem">Element not attached to any list of the same type. Can not be nullptr.</param>
+	///<param name="compare">Functor "compare" should return true, if the element satisfies the condition, and false otherwise.
+	///                      This functor is called for each element of a list.</param>
+	template<typename Func>
+	void insert_before(list_element_type* elem, Func&& compare) ATMOS_NONNULL(1);
+	
+	///<summary>Returns element after "elem" element.</summary>
+	///<param name="elem">Any element of any list. Can not be nullptr.</param>
+	///<returns>Element after "elem" element or nullptr if there is no element.</returns>
+	static list_element_type* next(list_element_type* elem) ATMOS_NONNULL(1);
+};
+
+template<typename ForwardListElement>
+void forward_list_tagged<ForwardListElement>::push_front(list_element_type* elem)
+{
+	forward_list_base::push_front(elem);
+}
+
+template<typename ForwardListElement>
+typename forward_list_tagged<ForwardListElement>::list_element_type*
+	forward_list_tagged<ForwardListElement>::pop_front()
+{
+	return static_cast<list_element_type*>(forward_list_base::pop_front());
+}
+
+template<typename ForwardListElement>
+bool forward_list_tagged<ForwardListElement>::remove(list_element_type* elem)
+{
+	return forward_list_base::remove(elem);
+}
+
+template<typename ForwardListElement>
+typename forward_list_tagged<ForwardListElement>::list_element_type*
+	forward_list_tagged<ForwardListElement>::first()
+{
+	return static_cast<list_element_type*>(first_element);
+}
+
+template<typename ForwardListElement>
+template<typename Func>
+void forward_list_tagged<ForwardListElement>::insert_before(list_element_type* elem, Func&& compare)
+{
+	forward_list_element* current = first_element;
+	forward_list_element* prev = nullptr;
+	while(current)
+	{
+		if(compare(static_cast<contained_type*>(static_cast<list_element_type*>(current))))
+			break;
+
+		prev = current;
+		current = current->next;
+	}
+		
+	return forward_list_base::insert_before(prev, current, elem);
+}
+
+template<typename ForwardListElement>
+typename forward_list_tagged<ForwardListElement>::list_element_type*
+	forward_list_tagged<ForwardListElement>::next(list_element_type* elem)
+{
+	return static_cast<list_element_type*>(elem->next);
+}
+
+} //namespace container
+} //namespace atmos
